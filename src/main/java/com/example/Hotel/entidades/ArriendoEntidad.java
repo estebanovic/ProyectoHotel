73889fb0ab925c426b5bc8d660e2d74ac2c01c7b
daghonet09 <75320061+daghonet09@users.noreply.github.com>
2021-01-12 08:41:38 -0300

    Create ArriendoEntidad.java

---
+++ src/main/java/com/example/Hotel/entidades/ArriendoEntidad.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.Hotel.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class ArriendoEntidad {
+    private String id;
+    private Date fechaIngreso;
+    private Date fechaRetiro;
+    private Cliente cliente;
+    private Pieza pieza;
+}
