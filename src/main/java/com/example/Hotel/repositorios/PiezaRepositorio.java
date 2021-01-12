2db5c8a98ce6d3e96dafd4e2181ecf1f4dcd5519
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-12 10:19:33 -0300

    Create PiezaRepositorio.java

---
+++ src/main/java/com/example/Hotel/repositorios/PiezaRepositorio.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.Hotel.repositorios;
+
+/**
+ *
+ * @author cris1
+ */
+public interface PiezaRepositorio {
+    
+}
