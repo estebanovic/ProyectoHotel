4ff331d7ae316b31e4b988246d8d8297fcba8ce6
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-01-12 12:10:03 -0300

    Update ArriendoServicio.java

--- src/main/java/com/example/Hotel/servicios/ArriendoServicio.java
+++ src/main/java/com/example/Hotel/servicios/ArriendoServicio.java
@@ -8,6 +8,8 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,6 +35,44 @@ public class ArriendoServicio {
         arriendo.setCotizacion(cotizacion);
         
         arriendoRepositorio.save(arriendo);
+    }
+    
+    public void editarArriendo(String id, Date fechaIngreso, Date fechaRetiro, Cliente cliente, Pieza pieza) throws Exception {
+        Optional<Arriendo> respuesta = arriendoRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            
+            LocalDate localFechaIngreso =fechaIngreso.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate localFechaRetiro =fechaIngreso.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        
+            int numDias = (int) ChronoUnit.DAYS.between(localFechaIngreso, localFechaRetiro);
+            int cotizacion = numDias * pieza.getPrecioNoche();
+        
+            Arriendo arriendo = respuesta.get();
+            arriendo.setFechaIngreso(fechaIngreso);
+            arriendo.setFechaRetiro(fechaRetiro);
+            arriendo.setNumDias(numDias);
+            arriendo.setCliente(cliente);
+            arriendo.setPieza(pieza);
+            arriendo.setCotizacion(cotizacion);
+
+            arriendoRepositorio.save(arriendo);
+        } else {
+            throw new Exception("ERROR");
+        }
+    }
+    public List<Arriendo> consultarArriendos() {
+        return arriendoRepositorio.findAll();
+    }
+    
+    public void eliminarArriendo(String id) throws Exception {
+        Optional<Arriendo> respuesta = arriendoRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            Arriendo arriendo = respuesta.get();
+            arriendoRepositorio.delete(arriendo);
+
+        } else {
+            throw new Exception("ERROR");
+        }
     }
 
 }
