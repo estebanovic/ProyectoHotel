aa1237e456783a47acd065efcd8532f6f01d863f
marco8246 <75321350+marco8246@users.noreply.github.com>
2021-01-12 10:31:24 -0300

    Update Cliente.java

--- src/main/java/com/example/Hotel/entidades/Cliente.java
+++ src/main/java/com/example/Hotel/entidades/Cliente.java
@@ -61,10 +61,7 @@ public class Cliente {
         this.mail = mail;
     }
 
-    @Override
-    public String toString() {
-        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", mail=" + mail + '}';
-    }
+   
 
     
 }
