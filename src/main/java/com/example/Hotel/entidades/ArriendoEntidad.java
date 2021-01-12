036cafeb69b0e45a25a375518b3ae0cf5b3df914
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-12 09:42:50 -0300

    Update ArriendoEntidad.java

--- src/main/java/com/example/Hotel/entidades/ArriendoEntidad.java
+++ src/main/java/com/example/Hotel/entidades/ArriendoEntidad.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.Hotel.entidades;
 
-/**
- *
- * @author cris1
- */
+import java.util.Date;
+
+
 public class ArriendoEntidad {
+    
+    private String id;
+    private Date fechaIngreso;
+    private Date fechaRetiro;
+    Cliente cliente;
+    Pieza pieza;
+    private int cotizacion;
+    
     
 }
