188aff77e3f1058999d0ad68df1ff4a8119e23f3
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-12 10:32:42 -0300

    Update ClienteRepositorio.java

--- src/main/java/com/example/Hotel/repositorios/ClienteRepositorio.java
+++ src/main/java/com/example/Hotel/repositorios/ClienteRepositorio.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.Hotel.repositorios;
 
-/**
- *
- * @author cris1
- */
-public interface ClienteRepositorio {
+import com.example.Hotel.entidades.Cliente;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepositorio extends JpaRepository<Cliente,String> {
     
+    @Query("SELECT a FROM Cliente a ORDER BY a.apellido, a.nombre")
+    List<Cliente> buscarTodosOrdenadoPorApellido();
 }
