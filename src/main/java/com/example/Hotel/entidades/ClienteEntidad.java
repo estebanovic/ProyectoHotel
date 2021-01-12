118f771976104b9ee22e7d00be8e8d53eec087e2
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-12 09:36:46 -0300

    Update ClienteEntidad.java

--- src/main/java/com/example/Hotel/entidades/ClienteEntidad.java
+++ src/main/java/com/example/Hotel/entidades/ClienteEntidad.java
@@ -6,4 +6,55 @@ public class ClienteEntidad {
     private String nombre;
     private String apellido;
     private int edad;
+    private String mail;
+
+    public ClienteEntidad(int id, String nombre, String apellido, int edad, String mail) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.mail = mail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+    
 }
