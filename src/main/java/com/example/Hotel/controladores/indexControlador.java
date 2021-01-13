b9d2c60e4518c5ace9616762c9f7ff0dbe35a4ee
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-01-13 10:27:43 -0300

    html

---
+++ src/main/java/com/example/Hotel/controladores/indexControlador.java
@@ -0,0 +1,19 @@
+
+package com.example.Hotel.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class indexControlador {
+    
+    
+    @GetMapping("/")
+    public String index(){
+        return"index.html";
+    }
+    
+}
