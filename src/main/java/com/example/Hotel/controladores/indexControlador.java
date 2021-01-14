225041e9f869f4c1590ad6d861ddf8ef11632b67
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-14 13:52:59 -0300

    Update indexControlador.java

--- src/main/java/com/example/Hotel/controladores/indexControlador.java
+++ src/main/java/com/example/Hotel/controladores/indexControlador.java
@@ -1,18 +1,15 @@
 
 package com.example.Hotel.controladores;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
-@RequestMapping("/")
+@RestController
 public class indexControlador {
-    
-    
-    @GetMapping("")
-    public String index(){
-        return"index.html";
+
+    @RequestMapping("/index.html")
+    public String home(){
+        return "index.html";
+    }
 }
-}
+
