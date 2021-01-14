cb84fed65270ef9f4dc40f25e92da936fc5e4398
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-14 18:28:47 -0300

    Update aboutControlador.java

--- src/main/java/com/example/Hotel/controladores/aboutControlador.java
+++ src/main/java/com/example/Hotel/controladores/aboutControlador.java
@@ -6,9 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class aboutControlador {
+<<<<<<< HEAD
 
     @RequestMapping("/index/about.html")
     public String home(){
         return "about.html";
     }
 }
+
+    
+    @GetMapping("/about")
+    public String about(){
+        return"about.html";
+    }
+}
+>>>>>>> 77f1037bfb1d1b61d07476f07554b489bdfda5d7
