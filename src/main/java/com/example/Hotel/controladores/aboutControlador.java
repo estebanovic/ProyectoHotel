87872d4cc34576f9236b16c6a6d4c9eb834526f0
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-14 18:26:51 -0300

    Update aboutControlador.java

--- src/main/java/com/example/Hotel/controladores/aboutControlador.java
+++ src/main/java/com/example/Hotel/controladores/aboutControlador.java
@@ -1,22 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.Hotel.controladores;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
-@RequestMapping("/")
+@RestController
 public class aboutControlador {
-    
-    
-    @GetMapping("/about")
-    public String about(){
-        return"about.html";
-}
+
+    @RequestMapping("/index/about.html")
+    public String home(){
+        return "about.html";
+    }
 }
