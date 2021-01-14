2eddf5ede6f084161736815bacfdd65ee75c877b
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-14 18:28:51 -0300

    Create contactControlador.java

---
+++ src/main/java/com/example/Hotel/controladores/contactControlador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.Hotel.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class contactControlador {
+    
+    @GetMapping("/contact")
+    public String contact(){
+        return"contact.html";
+    }
+}
