c50863cb4a0ee97edd2b25b54ca60d235248b3b1
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-15 03:01:16 -0300

    Update aboutControlador.java

--- src/main/java/com/example/Hotel/controladores/aboutControlador.java
+++ src/main/java/com/example/Hotel/controladores/aboutControlador.java
@@ -1,19 +1,19 @@
 
 package com.example.Hotel.controladores;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class aboutControlador {
 
     @RequestMapping("/index/about.html")
     public String home(){
         return "about.html";
     }
-
-    
     @GetMapping("/about")
     public String about(){
         return"about.html";
