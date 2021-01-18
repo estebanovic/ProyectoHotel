92c99862968634f7514a49119977942203951777
Kauh7 <77334315+Kauh7@users.noreply.github.com>
2021-01-18 01:05:17 -0300

    Update ClienteServicio.java
    Validadores de datos para guardar y editar el cliente

--- src/main/java/com/example/Hotel/servicios/ClienteServicio.java
+++ src/main/java/com/example/Hotel/servicios/ClienteServicio.java
@@ -13,8 +13,33 @@ public class ClienteServicio {
     @Autowired
     private ClienteRepositorio clienteRepositorio;
 
-    public void crearCliente(String id, String nombre, String apellido, Integer edad, String mail) {
+    public void crearCliente(String id, String nombre, String apellido, Integer edad, String mail) throws Exception{
         Cliente cliente = new Cliente();
+        
+        try {
+            if(cliente.getId()==null || cliente.getId().isEmpty()){
+            throw new Exception ("El cliente debe tener una ID valida");
+        }
+        if(cliente.getNombre()==null || cliente.getNombre().isEmpty()){
+            throw new Exception ("El cliente debe indicar un NOMBRE valido");
+        }
+        
+        if(cliente.getApellido()==null || cliente.getApellido().isEmpty()){
+            throw new Exception ("El cliente debe indicar un APELLIDO valido");
+        }
+        
+        if(cliente.getEdad()==null || cliente.getEdad()<18){
+            throw new Exception("El cliente debe tener una EDAD VALIDA");
+        }
+        
+        if(cliente.getMail()==null || cliente.getMail().isEmpty()){
+            throw new Exception("El cliente debe indicar el CORREO ELECTRONICO");
+        }
+        
+        if(cliente.getMail().contains("@")==false){
+            throw new Exception("El cliente debe indicar un CORREO ELECTRONICO valido");
+        }
+            
         cliente.setId(id);
         cliente.setNombre(nombre);
         cliente.setApellido(apellido);
@@ -22,20 +47,51 @@ public class ClienteServicio {
         cliente.setMail(mail);
 
         clienteRepositorio.save(cliente);
+        } catch (Exception e) {
+            throw new Exception(" ERROR AL GUARDAR EL CLIENTE ");
+
+        }
+   
     }
 
     public void editarCliente(String id, String nombre, String apellido, Integer edad, String mail) throws Exception {
         Optional<Cliente> respuesta = clienteRepositorio.findById(id);
         if (respuesta.isPresent()) {
             Cliente cliente = respuesta.get();
-            cliente.setNombre(nombre);
-            cliente.setApellido(apellido);
-            cliente.setEdad(edad);
-            cliente.setMail(mail);
+            
+    try {
+        if(cliente.getNombre()==null || cliente.getNombre().isEmpty()){
+            throw new Exception ("El cliente debe indicar un NOMBRE valido");
+        }
+        
+        if(cliente.getApellido()==null || cliente.getApellido().isEmpty()){
+            throw new Exception ("El cliente debe indicar un APELLIDO valido");
+        }
+        
+        if(cliente.getEdad()==null || cliente.getEdad()<18){
+            throw new Exception("El cliente debe tener una EDAD VALIDA");
+        }
+        
+        if(cliente.getMail()==null || cliente.getMail().isEmpty()){
+            throw new Exception("El cliente debe indicar el CORREO ELECTRONICO");
+        }
+        
+        if(cliente.getMail().contains("@")==false){
+            throw new Exception("El cliente debe indicar un CORREO ELECTRONICO valido");
+        }
+            
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setEdad(edad);
+        cliente.setMail(mail);
 
-            clienteRepositorio.save(cliente);
+        clienteRepositorio.save(cliente);
+        } catch (Exception e) {
+            throw new Exception(" ERROR AL GUARDAR DATOS DEL CLIENTE ");
+        }
+
         } else {
-            throw new Exception("ERROR");
+            throw new Exception("ERROR - CLIENTE NO ENCONTRADO");
         }
     }
 
@@ -50,7 +106,8 @@ public class ClienteServicio {
             clienteRepositorio.delete(cliente);
 
         } else {
-            throw new Exception("ERROR");
+            throw new Exception("ERROR - CLIENTE NO ENCONTRADO");
         }
     }
 }
+
