b0fadd4f344079eaad3d8c5c3e694b07fac16fdf
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-01-19 12:22:34 -0300

    crear cliente

--- src/main/java/com/example/Hotel/controladores/reservacionControlador.java
+++ src/main/java/com/example/Hotel/controladores/reservacionControlador.java
@@ -1,24 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.Hotel.controladores;
 
+import com.example.Hotel.servicios.ClienteServicio;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- *
- * @author Esteban
- */
+
 @Controller
 @RequestMapping("/")
 public class reservacionControlador {
+    
+    @Autowired
+    private ClienteServicio clienteServicio;
     
     @GetMapping("/reservacion")
     public String contact(){
         return"reservacion.html";
     }
+    
+    @PostMapping("/reservar")
+    public String reservar(@RequestParam String nombre,@RequestParam String apellido,@RequestParam String rut,@RequestParam Integer edad,@RequestParam String mail){
+        try {
+            clienteServicio.crearCliente(rut, nombre, apellido, edad, mail);
+        } catch (Exception ex) {
+            Logger.getLogger(reservacionControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return"reservacion.html";
+    }
+    
 }
