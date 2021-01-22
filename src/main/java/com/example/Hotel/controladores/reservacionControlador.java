e9ac71582f5b44ec23f4c24eafb71299e255d6a0
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-22 13:57:05 -0300

    Update reservacionControlador.java

--- src/main/java/com/example/Hotel/controladores/reservacionControlador.java
+++ src/main/java/com/example/Hotel/controladores/reservacionControlador.java
@@ -1,6 +1,10 @@
 package com.example.Hotel.controladores;
 
+import com.example.Hotel.entidades.Cliente;
+import com.example.Hotel.entidades.Pieza;
+import com.example.Hotel.servicios.ArriendoServicio;
 import com.example.Hotel.servicios.ClienteServicio;
+import com.example.Hotel.servicios.PiezaServicio;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,14 +23,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class reservacionControlador {
     
     @Autowired
+    private ArriendoServicio arriendoServicio;
+    
+    @Autowired
     private ClienteServicio clienteServicio;
+    
+    @Autowired
+    private PiezaServicio piezaServicio;
     
     @GetMapping("/reservacion")
     public String contact(){
         return"reservacion.html";
     }
     
-    @PostMapping("/reservar")
+    @PostMapping("/reservarr")
     public String reservar(ModelMap model, @RequestParam String nombre,@RequestParam String apellido,@RequestParam String rut,@RequestParam Integer edad,@RequestParam String mail){
         try {
             clienteServicio.crearCliente(rut, nombre, apellido, edad, mail);
@@ -35,5 +45,13 @@ public class reservacionControlador {
         }
         return"reservacion.html";
     }
-    
+    @PostMapping("/reservar")
+    public String reservar(ModelMap model, @RequestParam String id,@RequestParam Date fechaIngreso,@RequestParam Date fechaRetiro,@RequestParam Cliente cliente,@RequestParam Pieza pieza){
+        try {
+            arriendoServicio.crearArriendo("1", fechaIngreso, fechaRetiro,clienteServicio.BuscarCliente("20.930.652-2"), piezaServicio.BuscarPieza("1"));
+        } catch (Exception ex) {
+            Logger.getLogger(reservacionControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return"reservacion.html";
+    }
 }
