d32c336c94a10cf1824f0348e8880d27f706cec5
cristopher2354 <75314083+cristopher2354@users.noreply.github.com>
2021-01-22 13:57:02 -0300

    Update indexControlador.java

--- src/main/java/com/example/Hotel/controladores/indexControlador.java
+++ src/main/java/com/example/Hotel/controladores/indexControlador.java
@@ -1,17 +1,27 @@
 
 package com.example.Hotel.controladores;
 
+import com.example.Hotel.servicios.ArriendoServicio;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/") //localhost:8080/
 public class indexControlador {
 
+    
     @GetMapping("/")
     public String home(){
         return "index.html";
     }
+    
 }
 
